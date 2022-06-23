1f1004d26783f085c9c3482ecd28f86633d2c399
Himanshu Pal <himanshu.p19@iiits.in>
2022-06-23 17:48:26 +0530

    DSU

---
+++ DSU/207. Course Schedule.cpp
@@ -0,0 +1,24 @@
+// 207. Course Schedule
+// Problem link: https://leetcode.com/problems/course-schedule/
+
+/*
+This problem can be solved by using two methods:
+- DSU
+- Topological sorting
+*/
+
+bool canFinish(int n, vector<vector<int>> &prerequisites)
+{
+    vector<vector<int>> G(n);
+    vector<int> degree(n, 0), bfs;
+    for (auto &e : prerequisites)
+        G[e[1]].push_back(e[0]), degree[e[0]]++;
+    for (int i = 0; i < n; ++i)
+        if (!degree[i])
+            bfs.push_back(i);
+    for (int i = 0; i < bfs.size(); ++i)
+        for (int j : G[bfs[i]])
+            if (--degree[j] == 0)
+                bfs.push_back(j);
+    return bfs.size() == n;
+}
